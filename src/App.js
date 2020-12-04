5bc3109deb5ee6796c440a0cc349e92593161f5a
khurram4034 <64483246+khurram4034@users.noreply.github.com>
2020-12-04 18:16:57 +0500

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcoem to the Surge to Khurram</h1>
+      <h1>I am Creating my First React App</h1>
     </div>
   );
 }
